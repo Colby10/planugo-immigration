83ef9461801e02cd7305ba9b25b72b4d6203d79d
unknown <colbytoscani@gamail.com>
2023-09-06 14:22:03 +0100

    add

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
       <div className="App">
         {/*<Navbar/>*/}
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} /> 
 
 
           <Route path="/login" element={isNotLog(<Login />)} />
